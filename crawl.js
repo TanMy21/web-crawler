7ea1f197e6f2a4320675caa931c41a6c5785e323
TanMy21 <73067170+TanMy21@users.noreply.github.com>
2023-12-14 09:09:59 +0530

    normalize URL and getURLs

--- crawl.js
+++ crawl.js
@@ -1,3 +1,5 @@
+const { JSDOM } = require("jsdom");
+
 function normalizeURL(url) {
   const urlObj = new URL(url);
   let fullPath = `${urlObj.host}${urlObj.pathname}`;
@@ -7,6 +9,24 @@ function normalizeURL(url) {
   return fullPath;
 }
 
+function getURLsFromHTML(htmlBody, baseURL) {
+  const dom = new JSDOM(htmlBody);
+  const links = dom.window.document.querySelectorAll("a");
+  const urls = [];
+
+  for (const link of links) {
+    // Check if the href is a relative URL
+    if (link.href.startsWith("https:") || link.href.startsWith("/")) {
+      urls.push(new URL(link.href, baseURL).href);
+    } else {
+      continue;
+    }
+  }
+
+  return urls;
+}
+
 module.exports = {
   normalizeURL,
+  getURLsFromHTML,
 };
