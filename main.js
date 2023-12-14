1cc16152777ab9e9e5b8329f74cdb5da225702c0
TanMy21 <73067170+TanMy21@users.noreply.github.com>
2023-12-14 09:21:03 +0530

    main init

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-console.log("Hello World");
+const process = require("process");
+
+function main() {
+  const args = process.argv.slice(2); // Removes the first two elements
+
+  if (args.length < 1) {
+    console.error("Error: Too few arguments");
+    process.exit(1);
+  } else if (args.length > 1) {
+    console.error("Error: Too many arguments");
+    process.exit(1);
+  }
+
+  const baseURL = args[0];
+
+  // Your CLI logic here for the case of exactly 1 argument
+  console.log("Crawler starting at: ", baseURL);
+}
+
+main();
